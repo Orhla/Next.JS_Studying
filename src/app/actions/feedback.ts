8c03cb5aa5d7ba3ea47e71e10aa28f53742f6442
Vladimir Kromm <vkromm@gmail.com>
2026-05-14 18:17:27 +0200

    some live coding

--- src/app/actions/feedback.ts
+++ src/app/actions/feedback.ts
@@ -1,3 +1,6 @@
+"use server"
+import { prisma } from '@/lib/prisma'
+
 type ActionResult =
 | {success: true}
 | {success: false, error: string}
@@ -20,10 +23,41 @@ export async function submitFeedbackValidation(data: unknown): Promise<ActionRes
         if (!("weather" in data) || typeof data.weather !== 'string' || !validWeather.includes(data.weather)) {
             return {success: false, error: "Некорректный формат погоды. Погода может принимать значения: 'Солнечно', 'Облачно', 'Дождь'."}
         }
+        console.log("Saving to prisma")
+        await prisma.Feedback.create({
+            data: {
+                name: data.userName,
+                temperature: data.temperature,
+                weather: data.weather,
+            }
+        })
+        console.log("Saved to prisma")
+
 
         return {success: true}
 
     } catch (error) {
         return {success: false, error: `Ошибка сервера: ${error}`}
+    }
+}
+
+export async function saveFeedback(data: {
+    name: string
+    temperature: number
+    weather: string
+}): Promise<ActionResult> {
+    try {
+        if (data.name.length < 2) {
+            return { success: false, error: 'Имя слишком короткое' }
+        }
+        if (data.temperature < -50 || data.temperature > 50) {
+            return { success: false, error: 'Температура вне допустимого диапазона' }
+        }
+
+        await prisma.feedback.create({ data })
+
+        return { success: true }
+    } catch {
+        return { success: false, error: 'Ошибка сервера' }
     }
 }
